ec65138b9fc3f5298cf7772b8ce5cde2e340a276
弭忧萌虾 <2211479860@qq.com>
2024-04-16 21:12:18 +0800

    delete

--- src/main/java/lobster/moe/anvilcraft_delight/AnvilCraft_DelightDataGenerator.java
+++ src/main/java/lobster/moe/anvilcraft_delight/AnvilCraft_DelightDataGenerator.java
@@ -2,7 +2,6 @@ package lobster.moe.anvilcraft_delight;
 
 import lobster.moe.anvilcraft_delight.datagen.*;
 import lobster.moe.anvilcraft_delight.features.ModTreeFeatures;
-import lobster.moe.anvilcraft_delight.features.ModTreePlacements;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.core.RegistrySetBuilder;
@@ -22,6 +21,5 @@ public class AnvilCraft_DelightDataGenerator implements DataGeneratorEntrypoint 
     @Override
     public void buildRegistry(RegistrySetBuilder RegistryBuilder){
         RegistryBuilder.add(Registries.CONFIGURED_FEATURE, ModTreeFeatures::bootstrap);
-        RegistryBuilder.add(Registries.PLACED_FEATURE, ModTreePlacements::bootstrap);
     }
 }
